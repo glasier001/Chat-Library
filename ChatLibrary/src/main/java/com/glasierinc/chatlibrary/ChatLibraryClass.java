4d4694487207beb31e1155250d26b9db17d72c3c
GlasierInc <android@glasierinc.com>
2023-04-05 15:22:33 +0530

    First Commit

---
+++ ChatLibrary/src/main/java/com/glasierinc/chatlibrary/ChatLibraryClass.java
@@ -0,0 +1,18 @@
+package com.glasierinc.chatlibrary;
+
+import android.content.Context;
+
+public class ChatLibraryClass {
+
+//    public static void s(Context c,String message){
+//
+//        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+//
+//    }
+
+
+
+    public static void s(Context c){
+
+    }
+}
---
+++ app/src/main/java/com/glasierinc/chatmodule/MainActivity.java
@@ -0,0 +1,17 @@
+package com.glasierinc.chatmodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.glasierinc.chatlibrary.ChatLibraryClass;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ChatLibraryClass.s(this);
+    }
+}
