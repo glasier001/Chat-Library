2fdf9dcebfa6c120cbdc0168f58dacfd55ce156d
GlasierInc <android@glasierinc.com>
2023-04-07 11:45:40 +0530

    removed from build .gradle chat module

--- app/src/main/java/com/glasierinc/chatmodule/MainActivity.java
+++ app/src/main/java/com/glasierinc/chatmodule/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.glasierinc.chatlibrary.chat.activity.ClassRoomListActivity;
+//import com.glasierinc.chatlibrary.chat.activity.ClassRoomListActivity;
 
 
 public class MainActivity extends AppCompatActivity {
